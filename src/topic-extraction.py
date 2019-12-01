e23cfdd09ed0090c5d97d0a233258dbef8055433
Tess <tess.badun@gmail.com>
2019-12-01 17:18:10 -0500

    stem terms before processing

--- src/topic-extraction.py
+++ src/topic-extraction.py
@@ -9,6 +9,28 @@ df.columns = ["clothing_id","age","title","review","rating", \
 
 df = df[[i is not np.nan for i in df.review]]
 
+#%%
+# TERM LEMMATIZATION/STEMMING
+# NOTE: DOWNLOAD corpora/wordnet AND models/punkt
+from nltk.stem import WordNetLemmatizer, PorterStemmer
+from nltk.tokenize import word_tokenize
+import re 
+
+def lemmatizeSentence(sentence):
+    token_words=word_tokenize(sentence)
+    porter=PorterStemmer()
+    stem_sentence=[]
+    for word in token_words:
+        word2 = re.sub('[^A-Za-z0-9]+',"",word)
+        if len(word2)>0 and not any(i.isnumeric() for i in word2):
+            stem_sentence.append(porter.stem(word2))
+            stem_sentence.append(" ")
+    return "".join(stem_sentence)
+
+
+df['review_lemma'] = df.review.apply(lambda x: lemmatizeSentence(x))
+
+# %%
 X = df.drop("recomend",axis=1)
 y = df["recomend"]
 
@@ -19,7 +41,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.2, random_s
 # VECTORIZED VOCABULARY
 from sklearn.feature_extraction.text import CountVectorizer
 count_vect = CountVectorizer()
-X_train_counts = count_vect.fit_transform(X_train.review)
+X_train_counts = count_vect.fit_transform(X_train.review_lemma)
 vocab = pd.DataFrame(X_train_counts.toarray(), \
     columns=count_vect.get_feature_names(), \
         index=X_train.index)
